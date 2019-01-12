79e676ff2d444a3f65f60cd7d6249d001c097af8
dtmacroh@ucalgary.ca <dtmacroh@ucalgary.ca>
2019-01-12 15:32:17 -0700

    learning angular...

---
+++ getting-started-ng5/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardListComponent } from './card-list/card-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    CardListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
