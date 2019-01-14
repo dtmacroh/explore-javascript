a9c0a7545ff5463bd77a71507fe848bffe3ef5c5
dtmacroh@ucalgary.ca <dtmacroh@ucalgary.ca>
2019-01-13 22:03:58 -0700

    creating new card inpu component

--- getting-started-ng5/src/app/app.module.ts
+++ getting-started-ng5/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { CardListComponent } from './card-list/card-list.component';
+import { NewCardInputComponent } from './new-card-input/new-card-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    CardListComponent
+    CardListComponent,
+    NewCardInputComponent
   ],
   imports: [
     BrowserModule,
