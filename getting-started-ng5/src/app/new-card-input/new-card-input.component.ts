751828d3a94f9753b0adef7f879f454eeb8c608d
dtmacroh@ucalgary.ca <dtmacroh@ucalgary.ca>
2019-01-13 22:07:33 -0700

    adding card input component into main screen

--- getting-started-ng5/src/app/new-card-input/new-card-input.component.ts
+++ getting-started-ng5/src/app/new-card-input/new-card-input.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-card-input',
   templateUrl: './new-card-input.component.html',
-  styleUrls: ['./new-card-input.component.sass']
+  styleUrls: ['./new-card-input.component.sass'],
+  host:{'class':'col-4'}
 })
 export class NewCardInputComponent implements OnInit {
 
