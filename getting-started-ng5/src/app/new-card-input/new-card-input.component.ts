edf9931f098d3a5f36e713fb28c7f7722ad9deb9
dtmacroh@ucalgary.ca <dtmacroh@ucalgary.ca>
2019-01-14 20:19:29 -0700

    adding event for adding card

--- getting-started-ng5/src/app/new-card-input/new-card-input.component.ts
+++ getting-started-ng5/src/app/new-card-input/new-card-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit,Output,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-new-card-input',
@@ -8,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCardInputComponent implements OnInit {
   public newCard: any = {text: ''};
+  
   constructor() { }
-
+  @Output() onCardAdd = new EventEmitter<string>();
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.code === "Enter" && this.newCard.text.length > 0) {
+      this.addCard(this.newCard.text);
+    }
+  }
+  addCard(text) {
+    this.onCardAdd.emit(text);
+    this.newCard.text = '';
+  }
   ngOnInit() {
   }
 
